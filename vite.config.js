287bd0aac3d6ed42a2e37182aa8295e5eab5b63a
Karamarandas <karamarandas03@gmail.com>
2025-05-04 00:54:19 +0300

    fix: update vite configuration and add vercel rewrites for routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [tailwindcss(), react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
